746e04d2d2cabe947b3066e910efaf3f836392f9
MohamedMoustir <itsmoustir@gmail.com>
2024-10-28 20:30:26 +0100

    declartion input and function in js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 
 
 
-
-
+const header = document.querySelector("header");
 
 // function Open mun
 let navbar = document.getElementById("navbar-hamburger")
@@ -12,14 +11,26 @@ function OpenMnue() {
   navbar.classList.toggle("hidden");
 }
 
-// function Open cards Add Tasks
+// function Open cards Add Tasks and add style in header
 let crud_modal = document.getElementById("crud-modal") 
 function openCardsTasks() {
     crud_modal.classList.toggle("hidden");
-document.getElementsByTagName("nav").classList=("blur-sm");
+    header.classList.toggle('blur-sm');
     
 }
+// function close cards add tasks and remove style
 function closeCardsTasks(){
     crud_modal.classList.toggle("hidden");
+    header.classList.toggle('blur-sm');
+
 
 }
+
+
+// get data in html
+let description = document.getElementById("description");
+let Titer = document.getElementById("Titer")
+let priorite = document.getElementById("priorite")
+let Status = document.getElementById("Status")
+let date_limite = document.getElementById("date_limite")
+const addTasks =document.getElementById("addTasks")
